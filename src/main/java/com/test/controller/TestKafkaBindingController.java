cce1f8699ac20174efc05f83505fa74177233986
leducanh123456 <forrestgump.nxl@gmail.com>
2023-03-30 22:44:58 +0700

    commit

--- src/main/java/com/test/controller/TestKafkaBindingController.java
+++ src/main/java/com/test/controller/TestKafkaBindingController.java
@@ -5,20 +5,16 @@ import com.test.dto.Order;
 import com.test.kafka.ProducerMessage;
 import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 @RestController
 public class TestKafkaBindingController {
 
-    //private static final Logger logger = LoggerFactory.getLogger(TestKafkaBindingController.class);
     @Autowired
     private ProducerMessage producerMessage;
 
@@ -29,41 +25,21 @@ public class TestKafkaBindingController {
     public String sendSMSToKafka() throws Exception {
         System.out.println("----------------START SEND-----------");
         String mes = "Sending message to kafka topic by cafeincode:" + new Random().nextInt(100);
-        //producerMessage.sendMessage(mes);
         sendMessageBinding(mes);
         System.out.println("----------------END SEND-------------");
         return "OK";
     }
 
-    public void sendMessage(String message) throws Exception {
-        String TOPIC_NAME = "test-kafka";
-        String PUBSUB_NAME = "orderpubsub";
-        DaprClient client = new DaprClientBuilder().build();
-
-        for (int i = 0; i <= 1; i++) {
-            int orderId = i;
-            Order order = new Order(orderId);
-            //logger.info(objectMapper.writeValueAsString(order));
-            // Publish an event/message using Dapr PubSub
-            client.publishEvent(PUBSUB_NAME, TOPIC_NAME, objectMapper.writeValueAsBytes(order)).block();
-            TimeUnit.MILLISECONDS.sleep(100);
-
-        }
-    }
-
     public void sendMessageBinding(String message) throws Exception {
         String BINDING_NAME = "checkout";
         String BINDING_OPERATION = "create";
-        int k =0;
+        int k = 0;
         DaprClient client = new DaprClientBuilder().build();
-        while(k< 3) {
-
+        while (k < 1) {
             Random random = new Random();
-            int orderId = random.nextInt(1000-1) + 1;
+            int orderId = random.nextInt(1000 - 1) + 1;
             Order order = new Order(orderId);
-            //Using Dapr SDK to invoke output binding
-            //logger.info(objectMapper.writeValueAsString(objectMapper.writeValueAsString(order)));
-            client.invokeBinding(BINDING_NAME, BINDING_OPERATION, objectMapper.writeValueAsString(order)).block();
+            client.invokeBinding(BINDING_NAME, BINDING_OPERATION, order).block();
             k++;
         }
     }
--- src/main/java/com/test/controller/TestKafkaPubSubController.java
+++ src/main/java/com/test/controller/TestKafkaPubSubController.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.dto.Order;
 import io.dapr.client.DaprClient;
 import io.dapr.client.DaprClientBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +14,7 @@ import java.util.concurrent.TimeUnit;
 @RestController
 public class TestKafkaPubSubController {
 
-   // private static final Logger logger = LoggerFactory.getLogger(TestKafkaBindingController.class);
+    // private static final Logger logger = LoggerFactory.getLogger(TestKafkaBindingController.class);
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -25,20 +23,20 @@ public class TestKafkaPubSubController {
     public String sendSMSToKafka() throws Exception {
         System.out.println("----------------START SEND-----------");
         String mes = "Sending message to kafka topic by cafeincode:" + new Random().nextInt(100);
-        //producerMessage.sendMessage(mes);
         sendMessage(mes);
         System.out.println("----------------END SEND-------------");
         return "OK";
     }
+
     public void sendMessage(String message) throws Exception {
         String TOPIC_NAME = "test-kafka";
         String PUBSUB_NAME = "orderpubsub";
         DaprClient client = new DaprClientBuilder().build();
 
-        for (int i = 0; i <= 1; i++) {
+        for (int i = 0; i < 1; i++) {
             int orderId = i;
             Order order = new Order(orderId);
-            client.publishEvent(PUBSUB_NAME, TOPIC_NAME, objectMapper.writeValueAsBytes(order)).block();
+            client.publishEvent(PUBSUB_NAME, TOPIC_NAME, order).block();
             TimeUnit.MILLISECONDS.sleep(100);
 
         }
